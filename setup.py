3fe2201209cdd77e5d415c5e696f8f1eaadad31f
DekieCheng <38547588+DekieCheng@users.noreply.github.com>
2024-05-22 17:35:36 +0800

    Update setup.py
    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="dekiefirstpymodule",  # 包名
-    version="0.2",  # 版本号
+    version="0.3",  # 版本号
     packages=find_packages(),  # 自动发现所有包
     install_requires=[
         # 列出你的依赖包
     ],
     author="dekie",  # 作者名
     author_email="dekie.cheng@flex.com",  # 作者邮箱
-    description="A brief description of your package dekiefirstpymodule",  # 简短描述
+    description="A brief description of your package dekiefirstpymodule build from Github on 0.3 version.",  # 简短描述
 )
