c39c896c1edd78a1c3976061b1d98e2a05ccd42a
DekieCheng <38547588+DekieCheng@users.noreply.github.com>
2024-05-29 10:15:03 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dekiefirstpymodule",  # 包名
-    version="0.4",  # 版本号
+    version="0.5",  # 版本号
     packages=find_packages(),  # 自动发现所有包
     install_requires=[
         # 列出你的依赖包
@@ -10,5 +10,5 @@ setup(
     author="dekie",  # 作者名
     author_email="dekie.cheng@flex.com",  # 作者邮箱
     description="A brief description of your package dekiefirstpymodule build from Github on 0.4 version.",  # 简短描述
-    long_description="This is my first pyton package uploaded from git hub."
+    long_description="This is my first pyton package uploaded from git hub, just only for learning purpose"
 )
